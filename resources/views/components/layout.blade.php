ebbd4b7a03a17fcb9182c710c36c465ae154539a
keplib <keplib@gmail.com>
2024-09-19 23:20:20 +0200

    day3

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,6 +16,12 @@
         </style>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-      <h1>Hello World!</h1>
+        <nav>
+            <a href="/home" >Home</a>
+            <a href="/about" >About</a>
+            <a href="/contact" >Contact</a>
+        </nav>
+      <!-- <?php echo $slot ?> -->
+       {{ $slot }}
     </body>
 </html>
