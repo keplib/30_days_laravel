e413f03056cb4471da38970765464b2cd13b4e57
keplib <keplib@gmail.com>
2024-10-04 14:36:02 +0200

    day14-15: pagination and seeders

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/contact', function () {
 
 Route::get('/jobs', function () {
 
-    $jobs = Job::with('employer')->get();
+    $jobs = Job::with('employer')->simplePaginate(3);
 
     return view('jobs', [
         'jobs' => $jobs
